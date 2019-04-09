b03649b9464147b5b7149747cad0fbb8bfbe637d
daisy <1104567023@qq.com>
2019-04-09 18:44:03 +0800

    加入音乐

--- src/page/app.vue
+++ src/page/app.vue
@@ -65,23 +65,23 @@ export default {
   mounted(){
    
     document.querySelector('body').style.height=window.screen.height+'px';
-    // document.addEventListener('DOMContentLoaded', () => { //--创建页面监听，
-    //   const audioAutoPlay =() => {
-    //     const audio = document.getElementById('audio');
-    //     document.addEventListener("WeixinJSBridgeReady", () => { //等待微信端页面加载完毕 触发音频播放
-    //       audio.play();
-    //     }, false);
-    //   }
-    //   audioAutoPlay();
-    // });
+    document.addEventListener('DOMContentLoaded', () => { //--创建页面监听，
+      const audioAutoPlay =() => {
+        const audio = document.getElementById('audio');
+        document.addEventListener("WeixinJSBridgeReady", () => { //等待微信端页面加载完毕 触发音频播放
+          audio.play();
+        }, false);
+      }
+      audioAutoPlay();
+    });
    
-    // document.addEventListener('touchstart',  () =>{ //--创建触摸监听，当浏览器打开页面时，触摸屏幕触发事件，进行音频播放
-    //   const audioAutoPlay = () =>{
-    //     const audio = document.getElementById('audio');
-    //     audio.play();
-    //   }
-    //   audioAutoPlay();
-    // });
+    document.addEventListener('touchstart',  () =>{ //--创建触摸监听，当浏览器打开页面时，触摸屏幕触发事件，进行音频播放
+      const audioAutoPlay = () =>{
+        const audio = document.getElementById('audio');
+        audio.play();
+      }
+      audioAutoPlay();
+    });
   }
 }
 </script>
